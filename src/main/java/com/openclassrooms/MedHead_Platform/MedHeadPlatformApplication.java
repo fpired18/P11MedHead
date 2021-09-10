31e2a0c92353607b6791da2e642a316cdc6d22b1
Frederic Pichot <fpired@wanadoo.fr>
2021-09-10 10:26:07 +0200

    Création du projet11

---
+++ src/main/java/com/openclassrooms/MedHead_Platform/MedHeadPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.MedHead_Platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedHeadPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedHeadPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openclassrooms/MedHead_Platform/MedHeadPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.MedHead_Platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedHeadPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
