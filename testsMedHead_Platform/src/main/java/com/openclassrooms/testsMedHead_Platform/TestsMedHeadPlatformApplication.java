b66780facb2bb8169239ce077a3b006c9d79a8b3
Frederic Pichot <fpired@wanadoo.fr>
2021-09-17 13:15:39 +0200

    Ajout test Api

---
+++ testsMedHead_Platform/src/main/java/com/openclassrooms/testsMedHead_Platform/TestsMedHeadPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.testsMedHead_Platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsMedHeadPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestsMedHeadPlatformApplication.class, args);
+	}
+
+}
